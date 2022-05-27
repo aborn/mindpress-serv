10c3d0f4e644b8bf61d90d1365c4befd7db22463
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-28 07:34:34 +0800

    init spring-boot framework

--- src/main/java/com/github/aborn/mindpress/domain/Content.java
+++ src/main/java/com/github/aborn/mindpress/domain/Content.java
@@ -1,33 +1,17 @@
-/*
- *  Copyright 2022-2022 Sancaiwulian Holding Ltd (苏州三才物联网科技有限公司).
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package com.github.aborn.mindpress.domain;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
-import com.sancaiwulian.smes.inf.base.BaseEntity;
+import com.github.aborn.mindpress.inf.base.BaseEntity;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
 import javax.persistence.*;
-import javax.validation.constraints.*;
-import java.sql.Timestamp;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 /**
  * @author aborn
- * @website https://sancaiwulian.com
  * @description /
  * @date 2022-05-28
  **/
--- src/main/java/com/github/aborn/mindpress/service/ContentService.java
+++ src/main/java/com/github/aborn/mindpress/service/ContentService.java
@@ -1,28 +1,12 @@
-/*
- *  Copyright 2022-2022 Sancaiwulian Holding Ltd (苏州三才物联网科技有限公司).
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package com.github.aborn.mindpress.service;
 
 import com.github.aborn.mindpress.domain.Content;
 import com.github.aborn.mindpress.service.dto.ContentDto;
 import com.github.aborn.mindpress.service.dto.ContentQueryCriteria;
 import org.springframework.data.domain.Pageable;
-import java.util.Map;
+
 import java.util.List;
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * @author aborn
@@ -73,11 +57,4 @@ public interface ContentService {
     */
     void deleteAll(Long[] ids);
 
-    /**
-    * 导出数据
-    * @param all 待导出的数据
-    * @param response /
-    * @throws IOException /
-    */
-    void download(List<ContentDto> all, HttpServletResponse response) throws IOException;
 }
